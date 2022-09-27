8bee297ce5b8a22b978e4a6c35a3c6cbf62acbcf
Admin <artyomshevchenkowarmgray@gmail.com>
2022-09-27 17:05:03 +0300

    Updates

---
+++ precache-manifest.6727fec3a2a2c4160c5d1eec2027c48e.js
@@ -0,0 +1,214 @@
+self.__precacheManifest = [
+  {
+    "revision": "0db4afcc5d765d65b72f",
+    "url": "/testDeploy/static/css/main.cf9689cb.chunk.css"
+  },
+  {
+    "revision": "0db4afcc5d765d65b72f",
+    "url": "/testDeploy/static/js/main.0db4afcc.chunk.js"
+  },
+  {
+    "revision": "93b1c1adcbfc6a5f8c15",
+    "url": "/testDeploy/static/js/1.93b1c1ad.chunk.js"
+  },
+  {
+    "revision": "d2dd5f7de0ac8d0cc4ca",
+    "url": "/testDeploy/static/js/runtime~main.d2dd5f7d.js"
+  },
+  {
+    "revision": "7563912a32aef26fce40c2e11dbf63ba",
+    "url": "/testDeploy/static/media/Lobster.7563912a.woff"
+  },
+  {
+    "revision": "c1f5f1d23b56469081bf0e9a0a447f0f",
+    "url": "/testDeploy/static/media/Lobster.c1f5f1d2.eot"
+  },
+  {
+    "revision": "2d942223eccba9af2e62e0f955671d29",
+    "url": "/testDeploy/static/media/Lobster.2d942223.ttf"
+  },
+  {
+    "revision": "d091ac3d2f3ccf773945bc93b703b8ed",
+    "url": "/testDeploy/static/media/BebasNeue Regular.d091ac3d.woff"
+  },
+  {
+    "revision": "f1659397f7c8840da028b6d2445a1354",
+    "url": "/testDeploy/static/media/BebasNeue Regular.f1659397.ttf"
+  },
+  {
+    "revision": "72c7a03a2f650843c753072052457603",
+    "url": "/testDeploy/static/media/opensans-regular-webfont.72c7a03a.woff"
+  },
+  {
+    "revision": "d65eb3d4ad8f8c96f358653c3a255af6",
+    "url": "/testDeploy/static/media/BebasNeue Regular.d65eb3d4.eot"
+  },
+  {
+    "revision": "84f23b7275ed613a7ec6eb3a6630d9ce",
+    "url": "/testDeploy/static/media/opensans-regular-webfont.84f23b72.ttf"
+  },
+  {
+    "revision": "a105cda50ada8b1d3c5a401a5411f8ae",
+    "url": "/testDeploy/static/media/BebasNeue Regular.a105cda5.svg"
+  },
+  {
+    "revision": "68ddf354c70340b72fc9b884935fc65e",
+    "url": "/testDeploy/static/media/Lobster.68ddf354.svg"
+  },
+  {
+    "revision": "3d155600b926405dc3b1c8acba3a27a7",
+    "url": "/testDeploy/static/media/accessories_0.3d155600.png"
+  },
+  {
+    "revision": "4d8daca55e384576fd1ec8a28fdc87ce",
+    "url": "/testDeploy/static/media/accessories_1.4d8daca5.png"
+  },
+  {
+    "revision": "eb0099dd1a0ffc83c0d936f2303dfac8",
+    "url": "/testDeploy/static/media/accessories_2.eb0099dd.png"
+  },
+  {
+    "revision": "c886bb403c743f17df2137838947192e",
+    "url": "/testDeploy/static/media/accessories_3.c886bb40.png"
+  },
+  {
+    "revision": "291ed1c6ed1dc136b38a0de195a51937",
+    "url": "/testDeploy/static/media/accessories_4.291ed1c6.png"
+  },
+  {
+    "revision": "7ced2a4d0a4aac929a0ee65f44f90998",
+    "url": "/testDeploy/static/media/accessories_5.7ced2a4d.png"
+  },
+  {
+    "revision": "0e45a408d8114f2c1be7d693d670d025",
+    "url": "/testDeploy/static/media/accessories_6.0e45a408.png"
+  },
+  {
+    "revision": "83ba484439b094001cea80c72fde63b4",
+    "url": "/testDeploy/static/media/backpack_1.83ba4844.png"
+  },
+  {
+    "revision": "33c1be7e028deb03a1def3721db541cc",
+    "url": "/testDeploy/static/media/backpack_2.33c1be7e.png"
+  },
+  {
+    "revision": "abeb0343e76c8a7a97c743befeef12e7",
+    "url": "/testDeploy/static/media/backpack_3.abeb0343.png"
+  },
+  {
+    "revision": "5c9529f2e12b9b53fd02b53e2f5ba3cd",
+    "url": "/testDeploy/static/media/backpack_4.5c9529f2.png"
+  },
+  {
+    "revision": "7d02345468ab1bb33894dec931e6c5bd",
+    "url": "/testDeploy/static/media/backpack_5.7d023454.png"
+  },
+  {
+    "revision": "3d0583f5627fa25a20092eb7bda348ea",
+    "url": "/testDeploy/static/media/backpack_6.3d0583f5.png"
+  },
+  {
+    "revision": "092137e6b5c103fd28867d1d1ea72f4e",
+    "url": "/testDeploy/static/media/handbags_0.092137e6.png"
+  },
+  {
+    "revision": "f5f16c8d5187a69ce52770e3a73bfea4",
+    "url": "/testDeploy/static/media/handbags_1.f5f16c8d.png"
+  },
+  {
+    "revision": "a06dc60d31356a38d1bfddc783036a47",
+    "url": "/testDeploy/static/media/handbags_2.a06dc60d.png"
+  },
+  {
+    "revision": "c57af3b3aac3a7dd839603adcb3a510c",
+    "url": "/testDeploy/static/media/handbags_3.c57af3b3.png"
+  },
+  {
+    "revision": "34f56802ba683b1c701fa11b4df4473b",
+    "url": "/testDeploy/static/media/handbags_4.34f56802.png"
+  },
+  {
+    "revision": "3646f56090e51753fd98f7baa01f30ef",
+    "url": "/testDeploy/static/media/men_shoes_1.3646f560.png"
+  },
+  {
+    "revision": "461da9478f394bfaeb52d272dc48051e",
+    "url": "/testDeploy/static/media/men_shoes_2.461da947.png"
+  },
+  {
+    "revision": "74014cee84a9db4d0501b4dc74061ae2",
+    "url": "/testDeploy/static/media/men_shoes_3.74014cee.png"
+  },
+  {
+    "revision": "03469b0144f5b8c9dda57a95c98f2673",
+    "url": "/testDeploy/static/media/men_shoes_4.03469b01.png"
+  },
+  {
+    "revision": "ccd034edea22f4676326c8fe53b1ecdf",
+    "url": "/testDeploy/static/media/men_shoes_5.ccd034ed.png"
+  },
+  {
+    "revision": "7449f749c665edec72215771db708319",
+    "url": "/testDeploy/static/media/men_shoes_6.7449f749.png"
+  },
+  {
+    "revision": "19e52e8354da2b9d35107dd97a7866c1",
+    "url": "/testDeploy/static/media/tshirt_1.19e52e83.png"
+  },
+  {
+    "revision": "07d9184d698c5a73c39edde3a796b70e",
+    "url": "/testDeploy/static/media/tshirt_2.07d9184d.png"
+  },
+  {
+    "revision": "d128a06d2702624de5e36e98d80a2e0e",
+    "url": "/testDeploy/static/media/tshirt_3.d128a06d.png"
+  },
+  {
+    "revision": "d6a354ee0fd402e36f8ed647f1899938",
+    "url": "/testDeploy/static/media/tshirt_4.d6a354ee.png"
+  },
+  {
+    "revision": "8b86ebc92d2538325438bfe97ca6e3c3",
+    "url": "/testDeploy/static/media/tshirt_5.8b86ebc9.png"
+  },
+  {
+    "revision": "7ae58fe24acec6607436bc333567aabf",
+    "url": "/testDeploy/static/media/tshirt_6.7ae58fe2.png"
+  },
+  {
+    "revision": "109c1805d3f3ae2a4d8140f74eceef8e",
+    "url": "/testDeploy/static/media/wallet_0.109c1805.png"
+  },
+  {
+    "revision": "c032a1eb55acc2f6d631fe5f19532934",
+    "url": "/testDeploy/static/media/wallet_1.c032a1eb.png"
+  },
+  {
+    "revision": "a22a76bd43e90d725de0caf78113e5b6",
+    "url": "/testDeploy/static/media/wallet_2.a22a76bd.png"
+  },
+  {
+    "revision": "11ef3b40d69e259354c332e5d194af69",
+    "url": "/testDeploy/static/media/wallet_3.11ef3b40.png"
+  },
+  {
+    "revision": "8c0a9ab0e5788aac62ebc22bb15d540e",
+    "url": "/testDeploy/static/media/wallet_4.8c0a9ab0.png"
+  },
+  {
+    "revision": "13a208cb7caa5273d58e1ea7072c6b10",
+    "url": "/testDeploy/static/media/wallet_5.13a208cb.png"
+  },
+  {
+    "revision": "0ce4ea7c80e5ee456b6b4bd7d0a0639d",
+    "url": "/testDeploy/static/media/wallet_6.0ce4ea7c.png"
+  },
+  {
+    "revision": "662b84542731b2fa30c11ed6be7983dc",
+    "url": "/testDeploy/static/media/wallet_7.662b8454.png"
+  },
+  {
+    "revision": "9260c0e203d1e7ddfeafa7afb72dbe72",
+    "url": "/testDeploy/index.html"
+  }
+];
